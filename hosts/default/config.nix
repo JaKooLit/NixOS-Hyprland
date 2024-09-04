40b91676e068a91c7e476e61ebdbc6f8145e281b
JaKooLit <ejhay.games@gmail.com>
2024-09-05 00:35:27 +0900

    dangit

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -53,13 +53,13 @@ boot = {
       tmpfsSize = "30%";
     };
     # Appimage Support
-    binfmt.registrations.appimage = {
-      wrapInterpreterInShell = false;
-      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-      recognitionType = "magic";
-      offset = 0;
-      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-      magicOrExtension = ''\x7fELF....AI\x02'';
+    #binfmt.registrations.appimage = {
+    #  wrapInterpreterInShell = false;
+    #  interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    #  recognitionType = "magic";
+    #  offset = 0;
+    #  mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    #  magicOrExtension = ''\x7fELF....AI\x02'';
     };
     plymouth.enable = true;
   };
