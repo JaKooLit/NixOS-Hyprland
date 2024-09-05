d0423fc0a64ac1474ba7f82d2f21d8c93c919926
JaKooLit <ejhay.games@gmail.com>
2024-09-06 01:25:18 +0900

    again

--- auto-install.sh
+++ auto-install.sh
@@ -152,6 +152,8 @@ else
   fi
 fi
 
+cd ..
+
  # Check for existing configs and copy if does not exist
 for DIR1 in gtk-3.0 Thunar xfce4; do
   DIRPATH=~/.config/$DIR1
@@ -176,14 +178,14 @@ sleep 2
 printf "\n${NOTE} You can start Hyprland by typing Hyprland (note the capital H!).\n"
 printf "\n${NOTE} It is highly recommended to reboot your system.\n\n"
 
-    # Prompt user to reboot
+# Prompt user to reboot
 read -rp "${CAT} Would you like to reboot now? (y/n): " HYP
 
-    if [[ "$HYP" =~ ^[Yy]$ ]]; then
-        sudo systemctl reboot
-    fi
+if [[ "$HYP" =~ ^[Yy]$ ]]; then
+    # If user confirms, reboot the system
+    sudo systemctl reboot
 else
-    # Print error message if neither package is installed
-    printf "\n${WARN} Hyprland failed to install. Please check Install-Logs...\n\n"
-    exit 1
+    # Print a message if the user does not want to reboot
+    echo "Reboot skipped."
 fi
+
