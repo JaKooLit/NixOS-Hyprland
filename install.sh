cef4ad720b009c28bb8d014ae607ede7e9877d1f
installer <installer@gmail.com>
2024-09-08 08:04:53 +0900

    small formatting fix

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ if command -v lspci > /dev/null 2>&1; then
 fi
 
 echo "-----"
-printf "\n%.0s" {1..2}
+printf "\n%.0s" {1..1}
 
 cp hosts/default/*.nix hosts/"$hostName"
 git config --global user.name "installer"
@@ -97,8 +97,10 @@ NIX_CONFIG="experimental-features = nix-command flakes"
 echo "-----"
 printf "\n%.0s" {1..2}
 
-echo "$NOTE Rebuilding NixOS..... sor pls be patient.."
+echo "$NOTE Rebuilding NixOS..... so pls be patient.."
+echo "-----"
 echo "$CAT In the meantime, go grab a coffee or stretch or something..."
+echo "-----"
 echo "$ERROR YES!!! YOU read it right.. you staring too much at your monitor ha ha... joke :)......"
 printf "\n%.0s" {1..2}
 
