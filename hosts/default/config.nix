1f80fa3132bbc1c369893ec4c09188e39905e518
JaKooLit <ejhay.games@gmail.com>
2024-09-18 13:57:21 +0900

    neovim will not be installed anymore. nvim config will not be offered to copy anymore in ~/.config . It is still there but wont be offered nor copied

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -152,7 +152,7 @@
 	  firefox.enable = true;
 	  git.enable = true;
     nm-applet.indicator = true;
-    neovim.enable = true;
+    #neovim.enable = true;
 
 	  thunar.enable = true;
 	  thunar.plugins = with pkgs.xfce; [
