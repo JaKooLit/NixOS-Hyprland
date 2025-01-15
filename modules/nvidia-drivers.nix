f989763ad001cec933fce84de776a01940673a15
JaKooLit <ejhay.games@gmail.com>
2025-01-15 13:44:51 +0900

    switched nvidia drivers to download from latest and not from stable

--- modules/nvidia-drivers.nix
+++ modules/nvidia-drivers.nix
@@ -57,7 +57,7 @@ in
     nvidiaSettings = true;
       
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
   };
 }
