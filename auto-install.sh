24ebd511bf86279d41fb0a5dda2d3d650fff2f50
JaKooLit <ejhay.games@gmail.com>
2025-01-31 00:54:18 +0900

    color coding final adjustment for today

--- auto-install.sh
+++ auto-install.sh
@@ -9,7 +9,7 @@ ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
 NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
 WARN="$(tput setaf 1)[WARN]$(tput sgr0)"
 CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
-ORANGE=$(tput setaf 4)
+ORANGE=$(tput setaf 5)
 YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
