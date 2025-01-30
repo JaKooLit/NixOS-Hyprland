b6caf51de8f40e9b151751dcd7024c9c60bfcd46
JaKooLit <ejhay.games@gmail.com>
2025-01-30 23:55:18 +0900

    updated install script. Mistyped

--- auto-install.sh
+++ auto-install.sh
@@ -82,13 +82,13 @@ echo "-----"
 printf "\n%.0s" {1..1}
 
 # Aylurs GTK Shell v1 installation option
-read -p "Do you want to add AGS (aylur's gtk shell) v1 for Desktop Overview Like? (Y/n): " answer
+read -p "${CAT} Do you want to add ${ORANGE}'AGS (aylur's gtk shell) v1'${RESET} for Desktop Overview Like? (Y/n): " answer
 
 answer=${answer:-Y}
 
 if [[ "$answer" == "n" || "$answer" == "N" ]]; then
     sed -i 's/^ags.url = "github:aylur\/ags\/v1";/#ags.url = "github:aylur\/ags\/v1";/' flake.nix
-    sed -i 's/^ags/#ags/' host/default/packages-fonts.nix
+    sed -i 's/^ags/#ags/' hosts/default/packages-fonts.nix
 fi
 
 echo "-----"
