7a43439b6f780e9dcd70f4f2801892262025ffb3
installer <installer@gmail.com>
2025-11-13 20:11:06 -0500

    Merged Tony,BTW no plugin tmux config
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   modules/home/terminals/tmux.nix
    	new file:   modules/home/terminals/tmux.nix.ori

--- modules/home/terminals/tmux.nix
+++ modules/home/terminals/tmux.nix
@@ -1,6 +1,5 @@
 # Tmux is a terminal multiplexer that allows you to run multiple terminal sessions in a single window.
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   programs.tmux = {
     enable = true;
     mouse = true;
@@ -13,18 +12,34 @@
 
     extraConfig = ''
               set-option -g status-position top
-              set -sg terminal-overrides ",*:RGB"
+              set -ga terminal-overrides ",*:RGB"
               set -g default-terminal "tmux-256color"
               set-option -g history-limit 5000
               set -g renumber-windows on
+              set -g set-clipboard on
+
+              unbind C-b
+              bind-key C-a send-prefix
 
               unbind %
               unbind '"'
 
+              # VIM Motion realated settings 
               bind-key h select-pane -L
               bind-key j select-pane -D
               bind-key k select-pane -U
               bind-key l select-pane -R
+
+              bind -n M-h select-pane -L
+              bind -n M-j select-pane -D
+              bind -n M-k select-pane -U
+              bind -n M-l select-pane -R
+
+              bind-key -T copy-mode-vi v send-keys -X begin-selection
+              bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+              bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+              unbind -T copy-mode-vi MouseDragEnd1Pane
+
 
               set -gq allow-passthrough on
               bind-key x kill-pane # skip "kill-pane 1? (y/n)" prompt
@@ -33,40 +48,49 @@
               bind-key -n C-S-Tab previous-window
               bind-key -n M-Tab new-window
 
+              bind -n M-1 select-window -t 1
+              bind -n M-2 select-window -t 2
+              bind -n M-3 select-window -t 3
+              bind -n M-4 select-window -t 4
+              bind -n M-5 select-window -t 5
+              bind -n M-6 select-window -t 6
+              bind -n M-7 select-window -t 7
+              bind -n M-8 select-window -t 8
+              bind -n M-9 select-window -t 9
 
-            # Start windows and panes index at 1, not 0.
-            setw -g pane-base-index 1
+              # Start windows and panes index at 1, not 0.
+              setw -g pane-base-index 1
+              
+              # Creating panes 
+              bind-key "|" split-window -h -c "#{pane_current_path}"
+              bind-key "\\" split-window -fh -c "#{pane_current_path}"
 
+              bind-key "-" split-window -v -c "#{pane_current_path}"
+              bind-key "_" split-window -fv -c "#{pane_current_path}"
 
-            bind-key "|" split-window -h -c "#{pane_current_path}"
-            bind-key "\\" split-window -fh -c "#{pane_current_path}"
+              bind -r C-j resize-pane -D 15
+              bind -r C-k resize-pane -U 15
+              bind -r C-h resize-pane -L 15
+              bind -r C-l resize-pane -R 15
 
-            bind-key "-" split-window -v -c "#{pane_current_path}"
-            bind-key "_" split-window -fv -c "#{pane_current_path}"
+              # 'c' to new window
+              bind-key  c new-window
 
-            bind -r C-j resize-pane -D 15
-            bind -r C-k resize-pane -U 15
-            bind -r C-h resize-pane -L 15
-            bind -r C-l resize-pane -R 15
+              # 'n' next  window
+              bind-key  n next-window
 
-            # 'c' to new window
-            bind-key  c new-window
+              # 'p' next  previous
+              bind-key  n previous-window
 
-            # 'n' next  window
-            bind-key  n next-window
+              unbind r
+              bind r source-file ~/.config/tmux/tmux.conf
 
-            # 'p' next  previous
-            bind-key  n previous-window
+              bind -r m resize-pane -Z
 
-            unbind r
-            bind r source-file ~/.config/tmux/tmux.conf
-
-            bind -r m resize-pane -Z
-
-            bind-key  t clock-mode
-            bind-key  q display-panes
-            bind-key  u refresh-client
-            bind-key  o select-pane -t :.+
+              bind-key  t clock-mode
+              bind-key  q display-panes
+              bind-key  u refresh-client
+              bind-key  o select-pane -t :.+
 
 
       ##### Display Popups #####
@@ -78,32 +102,52 @@
         -E "lazygit"
       bind C-n display-popup -E 'bash -i -c "read -p \"Session name: \" name; tmux new-session -d -s \$name && tmux switch-client -t \$name"'
       bind C-j display-popup -E "tmux list-sessions | sed -E 's/:.*$//' | grep -v \"^$(tmux display-message -p '#S')\$\" | fzf --reverse | xargs tmux switch-client -t"
-      #bind C-p display-popup -E "ipython"
-      #bind C-f display-popup \
-       # -w 80% \
-       # -h 80% \
-       # -E 'rmpc'
       bind C-r display-popup \
         -d "#{pane_current_path}" \
         -w 90% \
         -h 90% \
         -E "yazi"
-      bind C-z display-popup \
-        -w 90% \
-        -h 90% \
-        -E 'nvim ~/NixOS-Hyprland/flake.nix'
       bind C-t display-popup \
         -d "#{pane_current_path}" \
         -w 75% \
         -h 75% \
         -E "zsh"
 
-      ##### Display Menu #####
+      # Tokyonight Moon theme (from tmux.tony.conf)
+      set -g status "on"
+      set -g status-bg "#222436"
+      set -g status-justify "left"
+      set -g status-left-length "100"
+      set -g status-right-length "100"
 
-      bind d display-menu -T "#[align=centre]Dotfiles" -x C -y C \
-        "NixOS flake.nix"        f  "display-popup -E 'nvim ~/NixOS-Hyprland/flake.nix'" \
-        "NixOS core packages"    c  "display-popup -E 'nvim ~/NixOS-Hyprland/modules/packages.nix'" \
-        "       Exit"              q  ""
+      # Messages
+      set -g message-style "fg=#86e1fc,bg=#3a3f5a,align=centre"
+      set -g message-command-style "fg=#86e1fc,bg=#3a3f5a,align=centre"
+
+      # Panes
+      set -g pane-border-style "fg=#3a3f5a"
+      set -g pane-active-border-style "fg=#82aaff"
+
+      # Windows
+      set -g window-status-activity-style "fg=#c8d3f5,bg=#222436,none"
+      set -g window-status-separator ""
+      set -g window-status-style "fg=#c8d3f5,bg=#222436,none"
+
+      # Statusline - current window
+      set -g window-status-current-format "#[fg=#82aaff,bg=#222436] #I: #[fg=#c099ff,bg=#222436](✓) #[fg=#86e1fc,bg=#222436]#(echo '#{pane_current_path}' | rev | cut -d'/' -f-2 | rev) #[fg=#c099ff,bg=#222436]"
+
+      # Statusline - other windows
+      set -g window-status-format "#[fg=#82aaff,bg=#222436] #I: #[fg=#c8d3f5,bg=#222436]#W"
+
+      # Statusline - right side
+      set -g status-right "#[fg=#82aaff,bg=#222436,nobold,nounderscore,noitalics]#[fg=#222436,bg=#82aaff,nobold,nounderscore,noitalics] #[fg=#c8d3f5,bg=#3a3f5a] #W #{?client_prefix,#[fg=#c099ff],#[fg=#86e1fc]}#[bg=#3a3f5a]#{?client_prefix,#[bg=#c099ff],#[bg=#86e1fc]}#[fg=#222436] #[fg=#c8d3f5,bg=#3a3f5a] #S "
+
+      # Statusline - left side (empty)
+      set -g status-left ""
+
+      # Modes
+      set -g clock-mode-colour "#82aaff"
+      set -g mode-style "fg=#82aaff bg=#444a73 bold"
 
 
     '';
@@ -111,9 +155,6 @@
     plugins = with pkgs; [
       tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.sensible
-      tmuxPlugins.catppuccin
-      #tmuxPlugins.Dracula
-      #tmuxPlugins.tokyo-night-tmux
     ];
   };
 }
